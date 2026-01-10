0fe75af5b8a94c366191505b42c38aa1b0e92715
Kabilan Veerasingam <kabilan.veerasingam@gmail.com>
2026-01-10 17:13:11 -0500

    CI: soft-fail tflint + report-only trivy + best-effort SARIF

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0"
